cd05714fafbf984c019009303235636b47af76bb
Sen Cai <sencaichi@gmail.com>
2022-09-09 14:29:33 -0400

    first commit

---
+++ components/loading/loading.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+import { Player } from "@lottiefiles/react-lottie-player";
+import swimming from "./swimming.json";
+import { Loader } from "../../styles/utils.module.css";
