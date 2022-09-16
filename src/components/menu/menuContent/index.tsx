d4fd5ff18ea4851d20475593ac78556ecee2a206
Sen Cai <sencaichi@gmail.com>
2022-09-16 16:40:58 -0400

    styling: h1, h2, and page-container/about page

--- src/components/menu/menuContent/index.tsx
+++ src/components/menu/menuContent/index.tsx
@@ -1,33 +1,3 @@
-// import React, { useContext } from "react";
-// import { MenuContext } from "../menuManager";
-// import cn from "classnames";
-// import routes from "../../../config/routes";
-// import {
-//   BrowserRouter,
-//   Route,
-// } from "react-router-dom";
-// // import "../../../styles/home.scss";
-
-// import utilStyles from "../../../styles/utils.module.scss";
-
-// export default function MenuContent() {
-//   const { open } = useContext(MenuContext);
-//   return (
-//     <div className={utilStyles.menuHolder}>
-//       <div className={cn(utilStyles.menuInside, { open })}>
-//         <div className={utilStyles.menuNavContainer}>
-//           <ul className={utilStyles.internalNavLinks}>
-//           {routes.map((route) => (
-//                <a href={route.path}>{route.component}</a>
-//           ))
-//               }
-//           </ul>
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
-
 import React, { useContext } from "react";
 import { MenuContext } from "../menuManager";
 import { Facebook, Instagram, Youtube, Dribbble, Twitch } from "react-feather";
@@ -35,44 +5,13 @@ import cn from "classnames";
 import routes from "../../../config/routes";
 import Link from "next/link";
 
-// import "./style.scss";
-
 const internalLinks = [
   {
-    // url: routes[0].path,
-    // name: routes[0].name,
-    // component: routes[0].component,
-    // img:
-    //   "https://images.unsplash.com/photo-1615713170963-2595d2c721bb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80",
+
     url: "/about",
     name: "About",
   },
 ]
-//   {
-//     url: "#2",
-//     component: <span>Recognition</span>,
-//     img:
-//       "https://images.unsplash.com/photo-1534120247760-c44c3e4a62f1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2148&q=80",
-//   },
-//   {
-//     url: "#3",
-//     component: <span>Studio</span>,
-//     img:
-//       "https://images.unsplash.com/photo-1554941829-202a0b2403b8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80",
-//   },
-//   {
-//     url: "#4",
-//     component: <span>Contribution</span>,
-//     img:
-//       "https://images.unsplash.com/photo-1593697821028-7cc59cfd7399?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2100&q=80",
-//   },
-//   {
-//     url: "#5",
-//     component: <span>Career</span>,
-//     img:
-//       "https://images.unsplash.com/photo-1588200618450-3a5b1d3b9aa5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80",
-//   },
-// ];
 
 export default function MenuContent() {
   const { open } = useContext(MenuContext);
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -17,23 +17,23 @@ export default function About(props: any) {
     >
       <MenuManager>
         <Menu />
-      <div className="aboutFlex">
-        <h1 className="label">About</h1>
-          <div className="desc">
-            <h2>
-              <span style={{ fontStyle: "italic "}}>Welcome to my portfolio!</span>
-            </h2>
-            <p>
-              My name is Sen Cai and I'm a solution-driven, motivated {" "} <span style={{ fontStyle: "italic" }}>Full Stack Engineer</span>, born and raised in Queens, NY. I'm interested in creating accessible digital spaces that feel like home.
-            </p>
-            <p>
-              I'm a graduate of Fullstack Academy's Grace Hopper program, where I learned to build intiuitive applications using the PERN stack. Before learning to code, I majored in Marketing and worked in telecommunications. I also have tons of customer service experience.
-            </p>
-            <p>
-              When I'm not coding, I'm writing!
-            </p>
+        <div className="page-container">
+          <h2 className="label">About</h2>
+            <div className="desc">
+              <h2>
+                <span style={{ fontStyle: "italic "}}>Welcome to my portfolio!</span>
+              </h2>
+              <p>
+                My name is Sen Cai and I'm a solution-driven, motivated {" "} <span style={{ fontStyle: "italic" }}>Full Stack Engineer</span>, born and raised in Queens, NY. I'm interested in creating accessible digital spaces that feel like home.
+              </p>
+              <p>
+                I'm a graduate of Fullstack Academy's Grace Hopper program, where I learned to build intiuitive applications using the PERN stack. Before learning to code, I majored in Marketing and worked in telecommunications. I also have tons of customer service experience.
+              </p>
+              <p>
+                When I'm not coding, I'm writing!
+              </p>
+            </div>
           </div>
-      </div>
       </MenuManager>
     </motion.div>
 
